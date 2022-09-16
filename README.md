02d5098e088ae22851159e34cfc935d8bb181b19
AWildErin <iansharp1@live.co.uk>
2022-09-17 00:34:53 +0100

    feat: Init steam

---
+++ README.md
@@ -0,0 +1,10 @@
+# P2:CE SDK Launcher
+This is a simple SDK launcher for [Portal 2: Community Edition]()
+
+## Installation
+Head over to the latest release, and unzip it anyway and just launch it!
+
+## Building
+1. Extract [the latest Steam SDK](https://partner.steamgames.com/downloads/steamworks_sdk_155.zip)'s `sdk` folder content into libs/steam
+2. If you're on Windows you can open this folder with Visual Studio and it should be all set up correctly.
+3. Build and run!
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,37 @@
+#include "mainview.h"
+
+#include <steam_api.h>
+#include <stdlib.h> 
+
 #include <QApplication>
 #include <QIcon>
 #include <QFile>
 
-#include "mainview.h"
+// AppID which we will use for steam.
+// 440000 - P2CE
+// 1400890 - P2CE SDK
+// 1802710 - Momentum Mod
+constexpr int APP_ID = 440000;
+
+// Main application
+void shutdown_steam()
+{
+	SteamAPI_Shutdown();
+}
 
 int main(int argc, char** argv)
 {
+	// Set the env variables for this application because we don't need steam_appid.txt
+	qputenv( "SteamAppId", QString::number( APP_ID ).toLocal8Bit() );
+	qputenv( "SteamGameId", QString::number( APP_ID ).toLocal8Bit() );
+
+	// Call Steam
+	if ( !SteamAPI_Init() )
+	{
+		printf( "Fatal Error - Steam must be running to use this tool (SteamAPI_Init() failed).\n" );
+		return 1;
+	}
+
 	QApplication app( argc, argv );
 
 	QApplication::setAttribute( Qt::AA_DisableWindowContextHelpButton );
@@ -19,6 +45,8 @@ int main(int argc, char** argv)
 	auto pDialog = new ui::CMainView( nullptr );
 	pDialog->setWindowTitle( "P2:CE SDK Launcher" );
 	pDialog->show();
+
+	atexit( shutdown_steam );
 
 	return QApplication::exec();
 }
